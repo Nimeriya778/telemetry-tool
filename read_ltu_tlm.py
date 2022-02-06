002671fd3b4fdd129e154ceb7ac953e71dc9f98c
Elena Kotlyarova <elenakotlyrova@gmail.com>
2022-02-06 18:36:00 +0300

    feat: write PLS telemetry to the database

--- read_ltu_tlm.py
+++ read_ltu_tlm.py
@@ -38,14 +38,7 @@ with open(filename, "rb") as binary_file:
         pls = PlsTelemetry.load_from_packet(packet)
 
         tlm[channel].append((cutime, brd, chg, ldd, pls))
-    print(PACKET_COUNT)
-
-    # def format_elem(elem):
-# """
-# Formats the output element to three decimal places
-# """
-
-#   return f"{elem:.3f}"
+    print(f"Total number of packets is {PACKET_COUNT}")
 
 # To use sqlite3 module create an object that represents the database.
 conn = sqlite3.connect("ltu-tel.sqlite")
@@ -62,9 +55,12 @@ cur.execute(
         define as integer datatype (UNIX time). */
         cutime integer,
         brd_lt1 real, brd_lt2 real, brd_lt3 real, brd_lt4 real,
-        chg_vtcur1 real, chg_vscur real, chg_vsdiv real, chg_vtdiv1 real, 
+        chg_vtcur1 real, chg_vscur real, chg_vsdiv real, chg_vtdiv1 real,
         ldd_hv1 real, ldd_ldout1 real, ldd_lt1 real, ldd_lt2 real, ldd_lt3 real,
-        ldd_rt1 real, ldd_rt2 real, ldd_rt3 real)
+        ldd_rt1 real, ldd_rt2 real, ldd_rt3 real, pls_hvr1 real, pls_ldr1 real,
+        pls_ldr2 real, pls_hvr2 real, pls_i1 real, pls_ld1 real, pls_ld2 real,
+        pls_i2 real, pls_i3 real, pls_ld3 real, pls_ld4 real, pls_i4 real,
+        pls_hvf1 real, pls_ldf1 real, pls_ldf2 real, pls_hvf2 real)
         """
 )
 
@@ -72,37 +68,23 @@ for row in tlm["LTU1.1"]:
     cur.execute(
         """INSERT INTO telemetry_ltu11(
         cutime, brd_lt1, brd_lt2, brd_lt3, brd_lt4,
-        chg_vtcur1, chg_vtdiv1, chg_vscur, chg_vsdiv,
+        chg_vtcur1, chg_vscur, chg_vsdiv, chg_vtdiv1,
         ldd_hv1, ldd_ldout1, ldd_lt1, ldd_lt2, ldd_lt3,
-        ldd_rt1, ldd_rt2, ldd_rt3)
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""",
+        ldd_rt1, ldd_rt2, ldd_rt3, pls_hvr1, pls_ldr1,
+        pls_ldr2, pls_hvr2, pls_i1, pls_ld1, pls_ld2,
+        pls_i2, pls_i3, pls_ld3, pls_ld4, pls_i4,
+        pls_hvf1, pls_ldf1, pls_ldf2, pls_hvf2)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,
+        ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""",
         (int(row[0]),)
         + astuple(row[1])
         + astuple(row[2])
         + astuple(row[3])
+        + astuple(row[4]),
     )
 
 # Save (commit) the changes
 conn.commit()
-
-# cur.execute("DROP TABLE IF EXISTS pls_tlm")
-
-# cur.execute(
-#     """CREATE TABLE pls_tlm(
-#         id integer PRIMARY KEY,
-#         pls_hvr1 real, pls_ldr1 real, pls_ldr2 real, pls_hvr2 real,
-#         pls_i1 real, pls_ld1 real, pls_ld2 real, pls_i2 real, pls_i3 real,
-#         pls_ld3 real, pls_ld4 real, pls_i4 real, pls_hvf1 real,
-#         pls_ldf1 real, pls_ldf2 real, pls_hvf2 real)
-#         """
-# )
-
-# for row in tlm["LTU1.1"]:
-#     cur.execute('''INSERT INTO pls_tlm(
-#         PlsTelemetry)
-#     VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?''', row)
-
-# conn.commit()
 
 # Changes have been committed, so close the connection
 conn.close()
