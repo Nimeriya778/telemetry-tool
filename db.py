87c9cea9fe5cb3b61726a52a73f5fa29fb286b87
Elena Kotlyarova <elenakotlyrova@gmail.com>
2022-02-17 19:28:17 +0300

    refactor: extract db queries into functions

--- db.py
+++ db.py
@@ -3,72 +3,67 @@ Database handling
 """
 
 import sqlite3
-from sqlite3 import Connection, Cursor
+from sqlite3 import Connection
 from dataclasses import astuple
+from typing import Dict, List, Tuple
 
 # Create an object that represents the database
 conn = sqlite3.connect("ltu-tel.sqlite")
 
-tlm = {"LTU1_1": [], "LTU2_1": [], "LTU3_1": []}
 
-
-def drop_table(dict_key_name: str) -> Cursor:
+def drop_table(connection: Connection, data: Dict[str, List[Tuple]]) -> None:
     """
     Removes the table only if the table exists.
     Otherwise, it just ignores the statement and does nothing
     """
+    # Use a dictionary key as a table name
+    for key in data:
+        drop_script = f"DROP TABLE IF EXISTS {key}"
+        connection.cursor().execute(drop_script)
 
-    for table in dict_key_name:
-        drop_script = f"DROP TABLE IF EXISTS {table}"
-        return conn.cursor().execute(drop_script)
 
-
-def create_table(connection: Connection, table_name: str) -> Cursor:
+def create_table(connection: Connection, data: Dict[str, List[Tuple]]) -> None:
     """
     Creates a table to keep LTU telemetry data
     """
 
-    create_script = f"""CREATE TABLE {table_name}(
-    id integer PRIMARY KEY,
-    cutime integer, brd_lt1 real, brd_lt2 real, brd_lt3 real, brd_lt4 real,
-    chg_vtcur1 real, chg_vscur real, chg_vsdiv real, chg_vtdiv1 real,
-    ldd_hv1 real, ldd_ldout1 real, ldd_lt1 real, ldd_lt2 real, ldd_lt3 real,
-    ldd_rt1 real, ldd_rt2 real, ldd_rt3 real, pls_hvr1 real, pls_ldr1 real,
-    pls_ldr2 real, pls_hvr2 real, pls_i1 real, pls_ld1 real, pls_ld2 real,
-    pls_i2 real, pls_i3 real, pls_ld3 real, pls_ld4 real, pls_i4 real,
-    pls_hvf1 real, pls_ldf1 real, pls_ldf2 real, pls_hvf2 real
-    )"""
+    for key in data:
+        create_script = f"""CREATE TABLE {key}(
+        id integer PRIMARY KEY,
+        cutime integer, brd_lt1 real, brd_lt2 real, brd_lt3 real, brd_lt4 real,
+        chg_vtcur1 real, chg_vscur real, chg_vsdiv real, chg_vtdiv1 real,
+        ldd_hv1 real, ldd_ldout1 real, ldd_lt1 real, ldd_lt2 real, ldd_lt3 real,
+        ldd_rt1 real, ldd_rt2 real, ldd_rt3 real, pls_hvr1 real, pls_ldr1 real,
+        pls_ldr2 real, pls_hvr2 real, pls_i1 real, pls_ld1 real, pls_ld2 real,
+        pls_i2 real, pls_i3 real, pls_ld3 real, pls_ld4 real, pls_i4 real,
+        pls_hvf1 real, pls_ldf1 real, pls_ldf2 real, pls_hvf2 real
+        )"""
 
-    return connection.cursor().execute(create_script)
+        connection.cursor().execute(create_script)
 
 
-def insert_into_table(cursor_obj: Cursor) -> Cursor:
+def insert_into_table(connection: Connection, data: Dict[str, List[Tuple]]) -> None:
     """
     Inserts telemetry data into a table
     """
 
-    for row in cursor_obj:
-        cursor_obj.execute(
-            """INSERT INTO telemetry_ltu11(
-            cutime, brd_lt1, brd_lt2, brd_lt3, brd_lt4,
-            chg_vtcur1, chg_vscur, chg_vsdiv, chg_vtdiv1,
-            ldd_hv1, ldd_ldout1, ldd_lt1, ldd_lt2, ldd_lt3,
-            ldd_rt1, ldd_rt2, ldd_rt3, pls_hvr1, pls_ldr1,
-            pls_ldr2, pls_hvr2, pls_i1, pls_ld1, pls_ld2,
-            pls_i2, pls_i3, pls_ld3, pls_ld4, pls_i4,
-            pls_hvf1, pls_ldf1, pls_ldf2, pls_hvf2)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,
-            ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""",
-            (int(row[0]),)
-            + astuple(row[1])
-            + astuple(row[2])
-            + astuple(row[3])
-            + astuple(row[4]),
-        )
-
-
-# Save (commit) the changes
-conn.commit()
-
-# Changes have been committed, so close the connection
-conn.close()
+    for key, value in data.items():
+        insert_script = f"""INSERT INTO {key}(
+        cutime, brd_lt1, brd_lt2, brd_lt3, brd_lt4,
+        chg_vtcur1, chg_vscur, chg_vsdiv, chg_vtdiv1,
+        ldd_hv1, ldd_ldout1, ldd_lt1, ldd_lt2, ldd_lt3,
+        ldd_rt1, ldd_rt2, ldd_rt3, pls_hvr1, pls_ldr1,
+        pls_ldr2, pls_hvr2, pls_i1, pls_ld1, pls_ld2,
+        pls_i2, pls_i3, pls_ld3, pls_ld4, pls_i4,
+        pls_hvf1, pls_ldf1, pls_ldf2, pls_hvf2)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,
+        ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"""
+        for row in value:
+            connection.cursor().execute(
+                insert_script,
+                (int(row[0]),)
+                + astuple(row[1])
+                + astuple(row[2])
+                + astuple(row[3])
+                + astuple(row[4]),
+            )
--- read_ltu_tlm.py
+++ read_ltu_tlm.py
@@ -4,9 +4,7 @@
 Read telemetry and write it to database
 """
 
-from dataclasses import astuple
 import sys
-import sqlite3
 from typing import Dict, List, Tuple
 from ip import get_ltu_channel
 from cu import get_cutime
@@ -14,6 +12,7 @@ from brd import BrdTelemetry
 from chg import ChgTelemetry
 from ldd import LddTelemetry
 from pls import PlsTelemetry
+from db import conn, drop_table, create_table, insert_into_table
 
 # sys.argv is a list, which contains the command-line arguments passed to the script.
 # The first item of this list contains the name of the script itself.
@@ -41,50 +40,18 @@ with open(filename, "rb") as binary_file:
         tlm[channel].append((cutime, brd, chg, ldd, pls))
     print(f"Total number of packets is {PACKET_COUNT}")
 
-# To use sqlite3 module create an object that represents the database.
-conn = sqlite3.connect("ltu-tel.sqlite")
-cur = conn.cursor()
+# Convert keys, since SQL don't allow using dots in queries
+for key in tlm:
+    new_key = key.replace(".", "_")
+    tlm[new_key] = tlm.pop(key)
 
-cur.execute("DROP TABLE IF EXISTS telemetry_ltu11")
+drop_table(conn, tlm)
 
-# Create table
-cur.execute(
-    """CREATE TABLE telemetry_ltu11(
-        id integer PRIMARY KEY,
-        /* cutime is timestamp, \
-        since there is no timestamp datatype in sqlite,\
-        define as integer datatype (UNIX time). */
-        cutime integer,
-        brd_lt1 real, brd_lt2 real, brd_lt3 real, brd_lt4 real,
-        chg_vtcur1 real, chg_vscur real, chg_vsdiv real, chg_vtdiv1 real,
-        ldd_hv1 real, ldd_ldout1 real, ldd_lt1 real, ldd_lt2 real, ldd_lt3 real,
-        ldd_rt1 real, ldd_rt2 real, ldd_rt3 real, pls_hvr1 real, pls_ldr1 real,
-        pls_ldr2 real, pls_hvr2 real, pls_i1 real, pls_ld1 real, pls_ld2 real,
-        pls_i2 real, pls_i3 real, pls_ld3 real, pls_ld4 real, pls_i4 real,
-        pls_hvf1 real, pls_ldf1 real, pls_ldf2 real, pls_hvf2 real)
-        """
-)
+create_table(conn, tlm)
 
-for row in tlm["LTU1.1"]:
-    cur.execute(
-        """INSERT INTO telemetry_ltu11(
-        cutime, brd_lt1, brd_lt2, brd_lt3, brd_lt4,
-        chg_vtcur1, chg_vscur, chg_vsdiv, chg_vtdiv1,
-        ldd_hv1, ldd_ldout1, ldd_lt1, ldd_lt2, ldd_lt3,
-        ldd_rt1, ldd_rt2, ldd_rt3, pls_hvr1, pls_ldr1,
-        pls_ldr2, pls_hvr2, pls_i1, pls_ld1, pls_ld2,
-        pls_i2, pls_i3, pls_ld3, pls_ld4, pls_i4,
-        pls_hvf1, pls_ldf1, pls_ldf2, pls_hvf2)
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?,
-        ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)""",
-        (int(row[0]),)
-        + astuple(row[1])
-        + astuple(row[2])
-        + astuple(row[3])
-        + astuple(row[4]),
-    )
+insert_into_table(conn, tlm)
 
-# Save (commit) the changes
+# Save the changes
 conn.commit()
 
 # Changes have been committed, so close the connection
