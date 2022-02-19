985ae1c2b793f6c45544cc8ef975fdc2ac0a7419
Elena Kotlyarova <elenakotlyrova@gmail.com>
2022-02-19 17:20:19 +0300

    refactor: extract packet code into a module

---
+++ packet.py
@@ -0,0 +1,38 @@
+"""
+Packets reading
+"""
+
+from typing import BinaryIO, Dict, List, Tuple
+from ip import get_ltu_channel
+from cu import get_cutime
+from brd import BrdTelemetry
+from chg import ChgTelemetry
+from ldd import LddTelemetry
+from pls import PlsTelemetry
+
+PACKET_SIZE = 1092
+
+
+def get_telemetry(file: BinaryIO) -> Dict[str, List[Tuple]]:
+    """
+    Gets telemetry data from packets
+    """
+
+    tlm: Dict[str, List[Tuple]] = {"LTU1.1": [], "LTU2.1": [], "LTU3.1": []}
+
+    while packet := file.read(PACKET_SIZE):
+
+        try:
+            channel = get_ltu_channel(packet)
+        except KeyError:
+            continue
+
+        cutime = get_cutime(packet)
+        brd = BrdTelemetry.load_from_packet(packet)
+        chg = ChgTelemetry.load_from_packet(packet)
+        ldd = LddTelemetry.load_from_packet(packet)
+        pls = PlsTelemetry.load_from_packet(packet)
+
+        tlm[channel].append((cutime, brd, chg, ldd, pls))
+
+    return tlm
--- read_ltu_tlm.py
+++ read_ltu_tlm.py
@@ -5,40 +5,15 @@ Read telemetry and write it to database
 """
 
 import sys
-from typing import Dict, List, Tuple
-from ip import get_ltu_channel
-from cu import get_cutime
-from brd import BrdTelemetry
-from chg import ChgTelemetry
-from ldd import LddTelemetry
-from pls import PlsTelemetry
+from packet import get_telemetry
 from db import conn, drop_table, create_table, insert_into_table
 
 # sys.argv is a list, which contains the command-line arguments passed to the script.
 # The first item of this list contains the name of the script itself.
 filename = sys.argv[1]
 
-tlm: Dict[str, List[Tuple]] = {"LTU1.1": [], "LTU2.1": [], "LTU3.1": []}
-
-PACKET_COUNT = 0
-with open(filename, "rb") as binary_file:
-
-    while packet := binary_file.read(1092):
-        PACKET_COUNT += 1
-
-        try:
-            channel = get_ltu_channel(packet)
-        except KeyError:
-            continue
-
-        cutime = get_cutime(packet)
-        brd = BrdTelemetry.load_from_packet(packet)
-        chg = ChgTelemetry.load_from_packet(packet)
-        ldd = LddTelemetry.load_from_packet(packet)
-        pls = PlsTelemetry.load_from_packet(packet)
-
-        tlm[channel].append((cutime, brd, chg, ldd, pls))
-    print(f"Total number of packets is {PACKET_COUNT}")
+with open(filename, "rb") as file:
+    tlm = get_telemetry(file)
 
 # Convert keys, since SQL don't allow using dots in queries
 for key in tlm:
@@ -51,8 +26,5 @@ create_table(conn, tlm)
 
 insert_into_table(conn, tlm)
 
-# Save the changes
 conn.commit()
-
-# Changes have been committed, so close the connection
 conn.close()
