bdf4c866c173191ae080510c48909af0f2626524
Elena Kotlyarova <elenakotlyrova@gmail.com>
2022-08-26 11:11:23 +0300

    fix: run the script as Python package

---
+++ src/telemetry_tool/__main__.py
@@ -0,0 +1,8 @@
+"""
+Run the script
+"""
+
+from .read_ltu_tlm import main
+
+if __name__ == "__main__":
+    main()
--- src/telemetry_tool/read_ltu_tlm.py
+++ src/telemetry_tool/read_ltu_tlm.py
@@ -8,18 +8,20 @@ import sys
 from .packet import get_telemetry
 from .db import conn, drop_table, create_table, insert_into_table
 
+def main():
 # sys.argv is a list, which contains the command-line arguments passed to the script.
 # The first item of this list contains the name of the script itself.
-filename = sys.argv[1]
+    filename = sys.argv[1]
 
-with open(filename, "rb") as file:
-    tlm = get_telemetry(file)
+    with open(filename, "rb") as file:
+        tlm = get_telemetry(file)
 
-drop_table(conn, tlm)
+    drop_table(conn, tlm)
 
-create_table(conn, tlm)
+    create_table(conn, tlm)
 
-insert_into_table(conn, tlm)
+    insert_into_table(conn, tlm)
 
-conn.commit()
-conn.close()
+    conn.commit()
+    conn.close()
+
